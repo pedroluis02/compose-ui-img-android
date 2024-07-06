95e2e5cdcc28d66c82d764c456fca6ec05216211
Pedro Luis <pedroluisnr22@gmail.com>
2024-07-05 20:21:20 -0500

    feat: :sparkles: add simple TopAppBar with title

--- app/src/main/java/com/github/pedroluis02/composeimagessample/ui/ComposeApp.kt
+++ app/src/main/java/com/github/pedroluis02/composeimagessample/ui/ComposeApp.kt
@@ -2,18 +2,28 @@ package com.github.pedroluis02.composeimagessample.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.github.pedroluis02.composeimagessample.R
 import com.github.pedroluis02.composeimagessample.ui.theme.AndroidComposeImagesTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ComposeApp() {
     AndroidComposeImagesTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(id = R.string.title)) }
+                )
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
             Text(
                 stringResource(id = R.string.app_name),
                 modifier = Modifier.padding(innerPadding)
