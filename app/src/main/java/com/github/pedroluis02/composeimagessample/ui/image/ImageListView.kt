a051a872be9b52ea99e7c3ef24d98a888895be15
Pedro Luis <pedroluisnr22@gmail.com>
2024-07-20 23:40:45 -0500

    build: :wrench: add android manifest permission

---
+++ app/src/main/java/com/github/pedroluis02/composeimagessample/ui/image/ImageListView.kt
@@ -0,0 +1,50 @@
+package com.github.pedroluis02.composeimagessample.ui.images
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+
+@Composable
+fun ImageListView(
+    modifier: Modifier = Modifier,
+    viewModel: ImageListViewModel = viewModel(),
+) {
+    val state = viewModel.state.collectAsState()
+
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(120.dp),
+        contentPadding = PaddingValues(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = modifier
+    ) {
+        items(state.value.elements, key = { it.id }) { element ->
+            Card {
+                AsyncImage(
+                    model = element.url,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(2 / 3f)
+                        .clip(MaterialTheme.shapes.small)
+                )
+            }
+        }
+    }
+}
